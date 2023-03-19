7a1f4717a84fa8f1f16ed4d4e5f406532504d553
AngelDeCaldasD01 <angel.decaldas@driving01.com>
2023-03-19 09:40:51 +0100

    add index.js

--- index.js
+++ index.js
@@ -3,38 +3,45 @@
 const { expect } = require("@playwright/test");
 const {chromium} = require("playwright");
 const fs = require('fs');
+const { config } = require("process");
 
 const shops = [
-    // {
-    //     vendor: "Game",
-    //     url: "https://www.game.es/consola-playstation-5-playstation-5-183224",
-    //     checkStock: async ({page}) => {
-    //         const content = await page.textContent('.product-quick-actions');
-    //         return content.includes("Producto no disponible") == false;
-    //     }
-    // },
-    // {
-    //     vendor: "Corte Ingles",
-    //     url: "https://www.elcorteingles.es/videojuegos/A37046604-consola-playstation-5/",
-    //     checkStock: async ({page}) => {
-    //         // This fragment is to accept cookies
-    //         await page.click('text=Modifica tu configuración');
-    //         await page.click('text=Aceptar todas');
+    {
+        vendor: "Game",
+        url: "https://www.game.es/consola-playstation-5-playstation-5-183224",
+        checkStock: async ({page}) => {
+            const content = await page.textContent('.product-quick-actions');
+            return content.includes("Producto no disponible") == false;
+        }
+    },
+    {
+        vendor: "Corte Ingles",
+        url: "https://www.elcorteingles.es/videojuegos/A37046604-consola-playstation-5/",
+        checkStock: async ({page}) => {
+            // This fragment is to accept cookies
+            await page.click('text=Modifica tu configuración');
+            await page.click('text=Aceptar todas');
 
-    //         const content = await page.textContent('[id="js_add_to_cart_desktop"]');
-    //         return content.includes("Agotado temporalmente") == false;
-    //     }
-    // },
+            const content = await page.textContent('[id="js_add_to_cart_desktop"]');
+            return content.includes("Agotado temporalmente") == false;
+        }
+    },
     {
         vendor: "Media Markt",
         url: "https://www.mediamarkt.es/es/product/_consola-sony-ps5-825-gb-4k-hdr-blanco-1487016.html",
         checkStock: async ({page}) => {
             // This fragment is to accept cookies
             await page.click('text=Denegar todo');
+            
+            const content = await page.textContent('[data-product-online-status="NOT_IN_ASSORTMENT"]', {timeout: 5000})
+            .catch(() => {});
 
-            const content = await page.textContent('[data-product-online-status="NOT_IN_ASSORTMENT"]');
-            console.log(content);
-            return content.includes("NOT_IN_ASSORTMENT") == false;
+            if (content == undefined) {
+                return true;
+            }
+            else {
+                return content.includes("Este artículo no está disponible actualmente.") == false;
+            }
         }
     },
 ];
